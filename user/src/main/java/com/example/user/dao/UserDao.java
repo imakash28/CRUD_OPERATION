fcf838517befea6a229b43c310b2c0e439611a90
imakash28 <maurya.akm99@gmail.com>
2022-06-22 23:35:50 +0530

    deleted unwanted

--- user/src/main/java/com/example/user/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package com.example.user.dao;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.user.model.User;
-
-public interface UserDao extends JpaRepository<User, Long>{
-	
-
-}
